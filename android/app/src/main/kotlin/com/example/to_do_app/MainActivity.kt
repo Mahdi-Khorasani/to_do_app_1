be0b37327805d1bd782ce6a54c511d35ff1aac29
mahdi khorasani <mahdikhorasani9134@gmail.com>
2024-04-28 13:30:17 +0330

    comited

--- android/app/src/main/kotlin/com/example/to_do_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/to_do_app/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.to_do_app
 
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+//class MainActivity: FlutterActivity()
